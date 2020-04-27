091adeb1ccf1388ab56df0c03d20f15956e1c45e
Sasa Radovanovic <salle90ar@gmail.com>
2020-04-27 23:47:52 +0200

    About section

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ const toggleNav = () => {
 
 burgerMenu.addEventListener('click', toggleNav);
 menuItemWrap.forEach((item) => item.addEventListener('click', toggleNav));
+
+// Intersection Observer
+const body = document.querySelector('#body');
+const about = document.querySelector('#work');
+
+const observer = new IntersectionObserver((entries) => {
+  if (entries[0].isIntersecting) {
+    body.classList.add('dark');
+  } else {
+    body.classList.remove('dark');
+  }
+});
+
+observer.observe(about);
